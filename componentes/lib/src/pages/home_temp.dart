364c9fdcfb79ae313cf9778b66072601ec43500d
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-03-28 13:16:30 -0600

    Inicio de proyecto de componentes

---
+++ componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Creamos un StatelessWidget
+class HomePageTemp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Temp'),
+    );
+  }
+}
