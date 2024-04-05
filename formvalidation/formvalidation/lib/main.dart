5f2ce5716e0db7a88d23efce8df09be8fd02e721
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-04-04 23:59:56 -0600

    Pages Bloc

--- formvalidation/formvalidation/lib/main.dart
+++ formvalidation/formvalidation/lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,14 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+
+      initialRoute: 'login',
+      routes: {
+        'login': ( (context) => LoginPage() ),
+        'home' : ( (context) => HomePage() ),
+      },
     );
   }
 }
---
+++ formvalidation/formvalidation/lib/src/pages/login_page.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
