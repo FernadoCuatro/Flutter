bb201d88542c3b3e00597502187b5ed998d2ffed
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-04-11 23:51:33 -0600

    Inherited Widget

--- formvalidation/formvalidation/lib/main.dart
+++ formvalidation/formvalidation/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
 import 'package:formvalidation/src/pages/home_page.dart';
 import 'package:formvalidation/src/pages/login_page.dart';
 
@@ -11,15 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
+    return Provider(
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
 
-      initialRoute: 'login',
-      routes: {
-        'login': ( (context) => LoginPage() ),
-        'home' : ( (context) => HomePage() ),
-      },
+        initialRoute: 'login',
+        routes: {
+          'login': ( (context) => LoginPage() ),
+          'home' : ( (context) => HomePage() ),
+        },
+      ),
     );
   }
 }
---
+++ formvalidation/formvalidation/lib/src/bloc/provider.dart
@@ -0,0 +1,26 @@
+// Aqui viene el inheritedWidget
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/login_bloc.dart';
+
+class Provider extends InheritedWidget {
+  // Propiedad
+  final loginBloc = LoginBloc();
+
+  // Constructor
+  Provider({ Key? key, required Widget child })
+    : super(key: key, child: child);
+
+  // Sobre escribimos
+  // Mas o menos dice que al actualizarse debe notificar a sus hijos
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+  // Ocupamos la instancia del LoginBloc
+  // Que regrese el estado de esa instancia
+  static LoginBloc? of(BuildContext context) {
+    final Provider? parentProvider = context.dependOnInheritedWidgetOfExactType<Provider>();
+    return parentProvider?.loginBloc;
+  }
+}
